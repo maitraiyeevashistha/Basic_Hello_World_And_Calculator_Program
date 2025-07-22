8714440199131540ffd57f94b16fd4157e1201de
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-07-22 20:09:06 +0530

    Create "Hello_World !".cpp

---
+++ "Hello_World !".cpp
@@ -0,0 +1,21 @@
+//EXPERIMENT - 1
+//MAITRAIYEE VASHISTHA
+//ENTC-A3
+//PRN-24070123057  
+///Problem Statement: Write a simple HELLO WORLD program using C++.
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout <<"Hello World !\n" << endl ;
+  cout <<"This Is My First C++ Program !" << endl ;
+   
+  return 0;
+}
+
+/*Output- 
+  Hello World !
+  This Is My First C++ Program !
+  */

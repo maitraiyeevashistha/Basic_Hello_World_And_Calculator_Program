437d9aa432fa23e3c347c533e2886bdd4dec116f
maitraiyeevashistha <maitraiyeevashistha@gmail.com>
2025-07-22 20:05:18 +0530

    Create Inputting_Two_Numbers.cpp

---
+++ Inputting_Two_Numbers.cpp
@@ -0,0 +1,13 @@
+#include using namespace std;
+
+int main() { 
+   int num1, num2;
+
+   cout << "Enter first number";
+   cin >> num1;
+
+   cout << "Enter second number";
+   cin >> num2;
+
+   return 0;
+}
